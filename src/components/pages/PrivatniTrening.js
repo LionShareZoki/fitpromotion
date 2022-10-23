cc1712028338e5cb48c882409ddd4d533ee56793
Zoran <pujic.zoran@gmail.com>
2022-10-23 16:44:41 +0200

    started PrivatniTrening page

--- src/components/pages/PrivatniTrening.js
+++ src/components/pages/PrivatniTrening.js
@@ -1,15 +1,56 @@
 import "../../App.css";
 import Footer from "../Footer";
-import Square from "../Square";
+import "./PrivatniTrening.css";
+import andrea from "../../imgs/pexels-andrea.jpg";
 
 const PrivatniTrening = () => {
   return (
     <div>
-      <div className="wrapper">
-        <div className="row"></div>
-        <div className="row"></div>
-        <div className="row"></div>
-        <div className="row"></div>
+      <div className="wrapper11">
+        <div className="rec">
+          <div className="rectangle">
+            <h1 className="first-text">
+              Naša ideja je pružiti visoko kvalitetnu uslugu
+            </h1>
+          </div>
+        </div>
+        <div className="recc">
+          <div className="l">
+            <div className="holding-text">
+              <p>
+                Kako bi to uspjeli, svakodnevno se educiramo u smjeru fitnessa i
+                nutricionizma.
+              </p>{" "}
+              <p>
+                Do sada smo pomogli velikom broju ljudi da ostvare svoje fitness
+                ciljeve kroz znanstveno dokazane metode.
+              </p>
+            </div>
+          </div>
+          <div className="r">
+            <div className="imagee-holder">
+              <img src={andrea} alt="" className="imagee" />
+            </div>
+          </div>
+        </div>
+        <div className="rec">
+          a lot of texta lot of texta lot of texta lot of texta lot of texta lot
+          of texta lot of texta lot of texta lot of texta lot of texta lot of
+          texta lot of texta lot of texta lot of texta lot of texta lot of texta
+          lot of texta lot of texta lot of texta lot of texta lot of texta lot
+          of texta lot of texta lot of texta lot of texta lot of texta lot of
+          texta lot of texta lot of texta lot of texta lot of texta lot of texta
+          lot of texta lot of texta lot of texta lot of texta lot of texta lot
+          of texta lot of texta lot of texta lot of texta lot of texta lot of
+          texta lot of texta lot of texta lot of texta lot of texta lot of texta
+          lot of texta lot of texta lot of texta lot of texta lot of texta lot
+          of texta lot of texta lot of texta lot of texta lot of texta lot of
+          texta lot of texta lot of texta lot of texta lot of texta lot of texta
+          lot of texta lot of texta lot of texta lot of texta lot of texta lot
+          of texta lot of texta lot of texta lot of texta lot of texta lot of
+          texta lot of texta lot of texta lot of text
+        </div>
+        <div className="rec"></div>
       </div>
       <Footer />
     </div>
