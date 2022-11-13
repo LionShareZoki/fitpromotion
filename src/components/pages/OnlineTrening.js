24a470b2122b6c03732921d6cb103bdef94628ea
Zoran <pujic.zoran@gmail.com>
2022-11-13 15:32:24 +0100

    Added scroll animation on OnlineTrening

--- src/components/pages/OnlineTrening.js
+++ src/components/pages/OnlineTrening.js
@@ -23,8 +23,15 @@ import plan from "../../imgs/plan.png";
 import training from "../../imgs/training.png";
 import video from "../../imgs/video.png";
 import whatsapp from "../../imgs/whatsapp.png";
+import Aos from "aos";
+import "aos/dist/aos.css";
+import { useEffect } from "react";
 
 const OnlineTrening = (props) => {
+  useEffect(() => {
+    Aos.init({ duration: 1300 });
+  }, []);
+
   return (
     <div>
       <div className="wrapper1">
@@ -32,22 +39,26 @@ const OnlineTrening = (props) => {
           <h1 className="heading2">KAKO IZGLEDA ONLINE SURADNJA?</h1>
         </div>
         <Rectangle
+          data-aos="fade-up"
           text="Prvi korak je upoznavanje pri kojem ću ja kao trener saznati sve potrebne informacije o tebi kako bi mogli nastaviti dalje sa programom. Zanimati će me tvoja visina, težina, zanimanje kojim se baviš, tvoja starost i slične informacije."
           heading="UPOZNAVANJE"
         />
         <Rectangle
+          data-aos="fade-up"
           text="U ovom dijelu programa tvoj trener će napraviti detaljan plan treninga kojega će sastaviti baš prema informacijama koje je dobio o tebi."
           heading="PLAN TRENINGA"
         />
         <Rectangle
+          data-aos="fade-up"
           text="Prehrana je važan dio zdravog života. Kroz ovaj dio tvoj trener odnosno nutricionist će ti osmisliti plan prehrane iz tjedan u tjedan. Tako da nećeš imati izbora nego pratiti i držati se prehrane što će rezultirati brzom promjenom tvoga tjelesnog sastava i napposljetku promjenom tvoga izgleda."
           heading="PREHRANA"
         />
         <Rectangle
+          data-aos="fade-up"
           text="Kroz tjedne razgovore tvoj trener pratiti će vaš napredak i prema njemu imati bolje informacine za daljnji tok programa."
           heading="PRAĆENJE TRENINGA"
-        />
-        <div className="initial--questionnaire">
+        />{" "}
+        <div data-aos="fade-up" className="initial--questionnaire">
           <div className="card2">
             <div className="img--holder1">
               <img src={Arrow} alt="" />
@@ -78,10 +89,10 @@ const OnlineTrening = (props) => {
           </div>
         </div>
         <div className="training--benefits">
-          <div className="row">
+          <div data-aos="fade-up" className="row">
             <h2 className="heading2">ŠTO DOBIVAŠ?</h2>
           </div>
-          <div className="row">
+          <div data-aos="fade-up" className="row">
             <Square text="Bolji san" icon={dream} />
             <Square text="Unaprjeđenje kvalitete života" icon={lifeQuality} />
             <Square text="Više energije kroz dan" icon={jump} />
@@ -90,7 +101,7 @@ const OnlineTrening = (props) => {
               icon={plan}
             />
           </div>
-          <div className="row">
+          <div data-aos="fade-up" className="row">
             <Square text="Korištenje aplikacije 'FitProMotion'" icon={app} />
             <Square text="Video instrukcije za svaku vježbu" icon={video} />
             <Square
@@ -102,7 +113,7 @@ const OnlineTrening = (props) => {
               icon={goals}
             />
           </div>
-          <div className="row">
+          <div data-aos="fade-up" className="row">
             <Square text="Personalizirani program trenigna" icon={training} />
             <Square text="Primjere raznovrsne prehrane" icon={food} />
             <Square
@@ -112,10 +123,11 @@ const OnlineTrening = (props) => {
             <Square text="Podršku 24/7 preko whatsappa" icon={whatsapp} />
           </div>
         </div>
-
-        <div className="mobile--app">
-          <h1 className="heading2">APLIKACIJA ZA TRENING</h1>
-          <div className="row">
+        <div data-aos="fade-up" className="mobile--app">
+          <h1 data-aos="fade-up" className="heading2">
+            APLIKACIJA ZA TRENING
+          </h1>
+          <div data-aos="fade-up" className="row">
             <div className="holder-for-image">
               <h4 className="text5">S lakoćom prati svoje dnevne obveze</h4>
               <img className="app--img" src={app1} alt="" />
@@ -140,7 +152,7 @@ const OnlineTrening = (props) => {
             </div>
           </div>
           <div className="holder-for-text">
-            <h5 className="text5">
+            <h5 data-aos="fade-up" className="text5">
               Aplikacija “FitProMotion” dostupna je svim aktivnim online
               klijentima, a preko nje dobivaš personalizirani program treninga s
               brojem ponavljanja, radnim opterećenjem i brojem radnih serija u
@@ -151,7 +163,9 @@ const OnlineTrening = (props) => {
           </div>
         </div>
         <div className="price--list">
-          <h2 className="heading2">CJENIK USLUGA</h2>
+          <h2 data-aos="fade-up" className="heading2">
+            CJENIK USLUGA
+          </h2>
           <div className="row">
             <VerticalRectangle
               name="LITE"
